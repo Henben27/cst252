fd2408ee636b05cc3a7d484975a6bd7c96117d23
Henben27 <89794379+Henben27@users.noreply.github.com>
2022-02-09 15:03:37 -0800

    implemented site css

---
+++ lab5/js/lab.js
@@ -0,0 +1,11 @@
+var carName = "Taurus";
+undefined
+var carYear = 1995;
+undefined
+var currentYear = 2022;
+undefined
+var carAge = currentYear - carYear;
+undefined
+document.writeLn("My car is " + carAge + " years old");
+VM1606:1 My car is 27 years old
+undefined
