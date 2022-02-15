1c54c794b86870fb0d1d47dc64b73d250d913e8c
Henben27 <89794379+Henben27@users.noreply.github.com>
2022-02-14 17:47:47 -0800

    completed lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,11 +1,21 @@
-var carName = "Taurus";
-undefined
-var carYear = 1995;
-undefined
-var currentYear = 2022;
-undefined
-var carAge = currentYear - carYear;
-undefined
-document.writeLn("My car is " + carAge + " years old");
-VM1606:1 My car is 27 years old
-undefined
+/*
+* Author: Henry Wilkinson <hwilkinson@csumb.edu>
+* Created: Febuary 9th
+* License: Public Domain
+*/
+
+//Define all Variables
+ make = "Ford";
+ model = "Taurus";
+ color = "Blue";
+ year = "1995";
+
+//calculate age
+ age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
